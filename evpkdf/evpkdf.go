8599cfeb4392b1ae2b5f634fb307ef097cd909be
Tristan Colgate <tcolgate@gmail.com>
2016-05-02 15:52:49 +0100

    EVPKDF: Working EvpKDF, with a test

--- evpkdf/evpkdf.go
+++ evpkdf/evpkdf.go
@@ -6,16 +6,15 @@ import (
 	"io"
 )
 
-//crypto-js uses openssl's evpKDF by default
-func New(hash func() hash.Hash, password []byte, salt []byte, keysize int, iterations int) io.Reader {
+//New creates a key derivation function that should match the EVP kdf in crypto-js, which, in turn
+// should be compatible with openssl's EVP kdf
+func New(hash func() hash.Hash, password []byte, salt []byte, keysize int, iterations int) []byte {
 	hasher := hash()
 	derivedKey := []byte{}
 	block := []byte{}
 
-	derivedKeyWords := []uint16{}
-
 	// Generate key
-	for len(derivedKeyWords) < keySize {
+	for len(derivedKey) < keysize {
 		if len(block) != 0 {
 			io.Copy(hasher, bytes.NewBuffer(block))
 		}
@@ -33,7 +32,5 @@ func New(hash func() hash.Hash, password []byte, salt []byte, keysize int, itera
 
 		derivedKey = append(derivedKey, block...)
 	}
-	derivedKey.sigBytes = keySize * 4
-
-	return bytes.NewBuffer(derivedKey)
+	return derivedKey[0:keysize]
 }
